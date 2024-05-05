d73ecf3381eae094da7b739286da5c8447b94a59
Alex Kolpakov <akolpakov@tuble.pw>
2024-05-05 23:19:15 +0200

    Style updates

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
-import express from 'express';
-import cors from 'cors';
-import env from './config';
-import errorHandler from './errorHandler';
-import router from './routes';
+import express from 'express'
+import cors from 'cors'
+import env from './config'
+import errorHandler from './errorHandler'
+import router from './routes'
 
-
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(errorHandler);
-app.use(router);
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(errorHandler)
+app.use(router)
 
 app.listen(env.SERVER_PORT, () => {
-    console.log(`RESTful API is listening on port ${env.SERVER_PORT}`);
-});
+  console.log(`RESTful API is listening on port ${env.SERVER_PORT}`)
+})
--- src/grpcClients.ts
+++ src/grpcClients.ts
@@ -1,11 +1,10 @@
-import { ProductInfoClient } from '../generated/product_grpc_pb';
-import { QuoteServiceClient } from '../generated/quote_grpc_pb';
-import { OrderServiceClient } from '../generated/order_grpc_pb';
-import { credentials } from '@grpc/grpc-js';
+import { ProductInfoClient } from '../generated/product_grpc_pb'
+import { QuoteServiceClient } from '../generated/quote_grpc_pb'
+import { OrderServiceClient } from '../generated/order_grpc_pb'
+import { credentials } from '@grpc/grpc-js'
 
-import env from './config';
+import env from './config'
 
-
-export const productStub = new ProductInfoClient(env.CATALOG_GRPC_SERVER, credentials.createInsecure());
-export const quoteStub = new QuoteServiceClient(env.QUOTE_GRPC_SERVER, credentials.createInsecure());
-export const orderStub = new OrderServiceClient(env.QUOTE_GRPC_SERVER, credentials.createInsecure());
+export const productStub = new ProductInfoClient(env.CATALOG_GRPC_SERVER, credentials.createInsecure())
+export const quoteStub = new QuoteServiceClient(env.QUOTE_GRPC_SERVER, credentials.createInsecure())
+export const orderStub = new OrderServiceClient(env.QUOTE_GRPC_SERVER, credentials.createInsecure())
